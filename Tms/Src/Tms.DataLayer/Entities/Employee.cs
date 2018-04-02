0530e9403f62378baaf5068552e8ca2d1233a4be
Semyon Morozov <semyonmorozov@gmail.com>
2018-04-02 13:31:15 +0500

    Создал сущность сотрудника

---
+++ Tms/Src/Tms.DataLayer/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tms.DataLayer.Repositories.Interfaces;
+
+namespace Tms.DataLayer.Entities
+{
+    public class Employee : IDbEntity
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+    }
+}
