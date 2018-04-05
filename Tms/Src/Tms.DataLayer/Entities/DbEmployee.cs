58f036911ee6fec25c489007595b50747c3d3192
Semyon Morozov <semyonmorozov@gmail.com>
2018-04-05 17:12:40 +0500

    Переименовал

--- Tms/Src/Tms.DataLayer/Entities/DbEmployee.cs
+++ Tms/Src/Tms.DataLayer/Entities/DbEmployee.cs
@@ -3,11 +3,14 @@ using Tms.DataLayer.Repositories.Interfaces;
 
 namespace Tms.DataLayer.Entities
 {
-    public class Employee : IDbEntity
+    public class DbEmployee : IDbEntity
     {
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Patronymic { get; set; }
+        public string MidlleName { get; set; }
+        public string Role { get; set; }
+        public string PassHash { get; set; }
+
     }
 }
