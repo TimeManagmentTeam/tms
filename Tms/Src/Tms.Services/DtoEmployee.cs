83d889f4afd3f5ff003e19fc3afd77371027813e
Semyon Morozov <semyonmorozov@gmail.com>
2018-04-08 12:25:02 +0500

    Переименовал класс, убрал методы маппинга

---
+++ Tms/Src/Tms.Services/DtoEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tms.Services
+{
+    public class DtoEmployee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MidlleName { get; set; }
+        public string Role { get; set; }
+        public string PassHash { get; set; }
+
+        public static class TmsRole
+        {
+            public const string Admin = "Admin";
+            public const string Employee = "DtoEmployee";
+        }
+    }
+}
