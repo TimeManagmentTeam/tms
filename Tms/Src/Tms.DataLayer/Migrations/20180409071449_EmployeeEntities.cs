c3f67e7993401c50a9db06cdd683a617dba2f6da
Semyon Morozov <semyonmorozov@gmail.com>
2018-04-09 12:22:34 +0500

    Добавил миграцию для сотрудников

---
+++ Tms/Src/Tms.DataLayer/Migrations/20180409071449_EmployeeEntities.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Tms.DataLayer.Migrations
+{
+    public partial class EmployeeEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestEntities");
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeEntities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    PassHash = table.Column<string>(nullable: true),
+                    Role = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeEntities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeEntities");
+
+            migrationBuilder.CreateTable(
+                name: "TestEntities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    StringForAutoMapperTest = table.Column<string>(nullable: true),
+                    Test = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestEntities", x => x.Id);
+                });
+        }
+    }
+}
