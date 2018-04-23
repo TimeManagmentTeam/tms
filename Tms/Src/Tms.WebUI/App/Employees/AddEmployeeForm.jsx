b7b6b152b768b570075adbc8aec03fc33a0448fe
Semyon Morozov <semyonmorozov@gmail.com>
2018-04-23 15:38:39 +0500

    Добавил полей для ввода и просмотра

--- Tms/Src/Tms.WebUI/App/Employees/AddEmployeeForm.jsx
+++ Tms/Src/Tms.WebUI/App/Employees/AddEmployeeForm.jsx
@@ -15,9 +15,12 @@ export default class AddEmployeeModal extends React.Component {
     render() {
         return (<form className="add-employee-form" ref={this.getFormRef}>
             <div className="add-employee-form__first-line">
-                <input type="text" className="add-employee-form__input" placeholder="Фамилия" name="lastName" />
-                <input type="text" className="add-employee-form__input" placeholder="Имя" name="firstName" />
-                <input type="text" className="add-employee-form__input" placeholder="Отчество" name="middleName" />
+                <input type="text" className="add-employee-form__input" placeholder="FirstName" name="firstName" />
+                <input type="text" className="add-employee-form__input" placeholder="MiddleName" name="middleName" />
+                <input type="text" className="add-employee-form__input" placeholder="LastName" name="lastName" />
+                <input type="text" className="add-employee-form__input" placeholder="Role" name="role" />
+                <input type="text" className="add-employee-form__input" placeholder="Email" name="email" />
+                <input type="text" className="add-employee-form__input" placeholder="Pass" name="passHash" />
             </div>
             <div className="add-employee-form__second-line">
                 <div className="add-employee-form__ok-button" onClick={this.addEmployee}>Создать</div>
